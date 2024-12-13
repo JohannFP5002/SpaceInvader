4ac15c6190da94f7d76e479e9f8cee59c2852643
JohannFP5002 <fumeroperezjohann@gmail.com>
2024-12-13 13:42:36 +0000

    PrimerosCambios
    comeinzo en la UI y añadi los modelos de todas las entidades del juego

---
+++ Assets/Scripts/ControladorPrincipal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPrincipal : MonoBehaviour
+{
+    [SerializeField]
+    GameObject MenuPrincipal,ButtonStart,ButtonOptions,ButtonExit,Titulo;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
